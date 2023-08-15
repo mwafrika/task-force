552f8bba2b261feefdbc2bfb1a0c362395072e50
mwafrika <mwafrikajosue@gmail.com>
2023-08-15 09:00:09 +0300

    Add users schema

---
+++ backend/models/user.js
@@ -0,0 +1,17 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  name: { type: String, required: true },
+  email: { type: String, required: true, unique: true },
+  phone: { type: String, required: true, unique: true },
+  firstName: { type: String },
+  lastName: { type: String },
+  createdAt: { type: Date, default: Date.now },
+  updatedAt: { type: Date, default: Date.now },
+});
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
